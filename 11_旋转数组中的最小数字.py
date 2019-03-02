d96749ffcd531b9dafa4f814ad9aa55ae455f721
Alan-lqf <33478369+Alan-lqf@users.noreply.github.com>
2019-03-02 11:05:32 +0800

    Update 11_旋转数组中的最小数字.py

--- 11_旋转数组中的最小数字.py
+++ 11_旋转数组中的最小数字.py
@@ -27,4 +27,4 @@ def find_min_in_rotated_arr(numbers):
 
 if __name__ == "__main__":
     print(find_min_in_rotated_arr([3, 4, 5, 1, 2]))
-    print(find_min_in_rotated_arr([1, 1, 1, 1, 1]))
+    print(find_min_in_rotated_arr([1, 0, 1, 1, 1]))

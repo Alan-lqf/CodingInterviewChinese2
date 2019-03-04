e5cc1bb69ca0d6006655516a6cd0421513ce4179
Alan-lqf <33478369+Alan-lqf@users.noreply.github.com>
2019-03-04 17:25:41 +0800

    Update 13_机器人运动范围.py

--- 13_机器人运动范围.py
+++ 13_机器人运动范围.py
@@ -1,35 +1,25 @@
-def moving_count(matrix, k):
-    if not isinstance(matrix, list) or len(matrix) == 0 or \
-            not isinstance(matrix[0], list) or len(matrix[0]) == 0:
+def moving_count(rows, cols, k):
+    if rows == 0 or cols == 0:
         return False
 
-    rows = len(matrix)
-    cols = len(matrix[0])
-    for r in matrix:
-        if len(r) != cols:
-            return False
-
     visited = [[False] * cols for _ in range(rows)]
-    count = count_recursively(matrix, k, (0, 0), visited)
+    count = count_recursively(rows, cols, k, (0,0), visited)
     return count
 
 
-def count_recursively(matrix, k, matrix_indices, visited):
+def count_recursively(rows, cols, k, matrix_indices, visited):
     count = 0
     m_i, m_j = matrix_indices
-    if check(matrix, k, matrix_indices, visited):
-        count = count + count_recursively(matrix, k, (m_i + 1, m_j), visited) + \
-            count_recursively(matrix, k, (m_i - 1, m_j), visited) + \
-            count_recursively(matrix, k, (m_i, m_j + 1), visited) + \
-            count_recursively(matrix, k, (m_i, m_j - 1), visited)
+
+    if check(rows, cols, k, matrix_indices, visited):
+        visited[m_i][m_j] = True
+        count = 1 + count_recursively(rows, cols, k, (m_i+1, m_j), visited) + count_recursively(rows, cols, k, (m_i-1, m_j), visited) + count_recursively(rows, cols, k, (m_i, m_j+1), visited) + count_recursively(rows, cols, k, (m_i, m_j-1), visited)
     return count
 
 
-def check(matrix, k , matrix_indices, visited):
+def check(rows, cols, k, matrix_indices, visited):
     m_i, m_j = matrix_indices
-    if not (0 <= m_i < len(matrix)) or not (0 <= m_j < len(matrix[0])) or \
-            (sum_digit(m_i) + sum_digit(m_j) > k) or \
-            visited[m_i][m_j]:
+    if not (0 <= m_i < rows) or not (0 <= m_j < cols) or (sum_digit(m_i) + sum_digit(m_j) > k) or visited[m_i][m_j]:
         return False
     return True
 
@@ -40,3 +30,40 @@ def sum_digit(x):
         sum += x % 10
         x = x // 10
     return sum
+
+
+# ===============测试代码================
+
+def test(rows, cols, k, expected):
+    if moving_count(rows, cols, k) == expected:
+        print("Passed. \n")
+    else:
+        print("Faild. \n")
+
+# 多行多列
+def test1():
+    test(10, 10, 5, 21)
+
+# 一行部分
+def test2():
+    test(1, 100, 10, 29)
+
+
+# 一列部分
+def test3():
+    test(100, 1, 15, 79)
+
+
+# 不能进入任意一格
+def test4():
+    test(10, 10, -5, 0)
+
+
+
+
+if __name__ == '__main__':
+
+    test1()
+    test2()
+    test3()
+    test4()
